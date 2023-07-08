c5363e5376a050e8ece9975d66dd7fc89d855150
SalahuddinShayan <104673244+SalahuddinShayan@users.noreply.github.com>
2023-07-08 16:57:33 +0530

    profile test

--- src/main/java/com/myblog/Entity/Post.java
+++ src/main/java/com/myblog/Entity/Post.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Post {
 	
@@ -12,5 +18,20 @@ public class Post {
 	@GeneratedValue
 	@Column(name="postId")
 	private int postId;
-
+	
+	@Column(name="postTitle")
+	private String postTitle;
+	
+	@Column(name="post", columnDefinition = "text")
+	private String post;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private java.util.Date utilTimestamp;
+	
+	@Column(name="author")
+	private String author;
+	
+	@Column(name="read")
+	private int read;
+	
 }
